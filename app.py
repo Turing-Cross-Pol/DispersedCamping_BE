33b34eb119cdb1f0fbace0962ddbc646544dad52
Nathan Keller <keller.nathan@gmail.com>
2020-05-25 10:19:49 -0600

    Adjust Travis one mroe time

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# import models
+import models
 
 @app.route('/')
 def hello():
