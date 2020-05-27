aef1218b3afe56151ca5f66c14d2191fd6798ac7
Nathan Keller <keller.nathan@gmail.com>
2020-05-27 13:41:55 -0600

    Adjust whitespace in __init__.py
    Co-Authored-By: willkunz13 <53539480+willkunz13@users.noreply.github.com>

--- app/__init__.py
+++ app/__init__.py
@@ -13,43 +13,42 @@ db = SQLAlchemy()
 
 
 def create_app(config_name):
-				from app.models import Campsite
-				app = FlaskAPI(__name__, instance_relative_config=True)
-				app.config.from_object(app_config[config_name])
-				app.config.from_pyfile('config.py')
-				app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-				db.init_app(app)
+	from app.models import Campsite
+	app = FlaskAPI(__name__, instance_relative_config=True)
+	app.config.from_object(app_config[config_name])
+	app.config.from_pyfile('config.py')
+	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+	db.init_app(app)
 
 
-				@app.route('/campsites/', methods=['POST', 'GET'])
-				def campsites():
+	@app.route('/campsites/', methods=['POST', 'GET'])
+	def campsites():
 
 
-								if request.method == "POST":
-												name = str(request.data.get('name', ''))
-												if name:
-																campsite = Campsite(name=name)
-																campsite.save()
-																response = jsonify({
-																				'id': campsite.id,
-																				'name': campsite.name,
-																})
-																response.status_code = 201
-																return response
-								else:
-												# GET
-												campsites = Campsite.get_all()
-												results = []
+		if request.method == "POST":
+			name = str(request.data.get('name', ''))
+			if name:
+				campsite = Campsite(name=name)
+				campsite.save()
+				response = jsonify({
+								'id': campsite.id,
+								'name': campsite.name,
+								})
+				response.status_code = 201
+			return response
+		else:
+			# GET
+			campsites = Campsite.get_all()
+			results = []
 
-												for campsite in campsites:
-																obj = {
-																				'id': campsite.id,
-																				'name': campsite.name,
-																}
-																results.append(obj)
-																response = jsonify(results)
-																response.status_code = 200
-																return response
+			for campsite in campsites:
+				obj = {
+						'id': campsite.id,
+						'name': campsite.name,
+						}
+				results.append(obj)
+				response = jsonify(results)
+				response.status_code = 200
+			return response
 
-
-				return app
+	return app
