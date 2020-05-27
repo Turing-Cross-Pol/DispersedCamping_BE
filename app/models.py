bb73e002d5e8eabeee6c22b8871f178445d423bc
Nathan Keller <keller.nathan@gmail.com>
2020-05-27 09:40:48 -0600

    Create Comment Resource in database
    Co-Authored-By: willkunz13 <53539480+willkunz13@users.noreply.github.com>

--- app/models.py
+++ app/models.py
@@ -6,47 +6,43 @@ class Campsite(db.Model):
     __tablename__ = 'campsites'
 
     id = db.Column(db.Integer, primary_key=True)
-#    image_url = db.Column(db.String())
-#    city = db.Column(db.String())
-#    state = db.Column(db.String())
+    image_url = db.Column(db.String())
+    city = db.Column(db.String())
+    state = db.Column(db.String())
     name = db.Column(db.String())
-#    description = db.Column(db.String())
-#    driving_tips = db.Column(db.String())
-#    lon = db.Column(db.Float())
-#    lat = db.Column(db.Float())
-#    date_created = db.Column(db.DateTime, default=db.func.current_timestamp()
+    description = db.Column(db.String())
+    driving_tips = db.Column(db.String())
+    lon = db.Column(db.Float())
+    lat = db.Column(db.Float())
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    comments = db.relationship('Comment', backref='campsite', lazy=True)
 
-
-    def __init__(self, name):
-    #		self.image_url = image_url
-    #		self.city = city
-    #		self.state = state
-    		self.name = name
-    #		self.description = description
-    #		self.driving_tips = driving_tips
-    #		self.lon = lon
-    #		self.lat = lat 
-    
-    
     def save(self):
     	db.session.add(self)
     	db.session.commit()
-    
-    
+
+
     @staticmethod
     def get_all():
     		return Campsite.query.all()
-    
-    
+
+
     def delete(self):
     		db.session.delete(self)
     		db.session.commit()
-    
-    
+
+
     def __repr__(self):
     		return "<Campsite: {}>".format(self.name)
-    
 
-#class CampsiteSchema(ma.SQLAlchemyAutoSchema):
-#		class Meta:
-#				model = Campsite
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    title = db.Column(db.String())
+    description = db.Column(db.String())
+    rating = db.Column(db.String())
+    campsite_id = db.Column(db.Integer, db.ForeignKey('campsites.id'))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
