e28ab2b821da72c9c4a1b787efec862f6aa4f160
Nathan Keller <keller.nathan@gmail.com>
2020-05-28 14:44:32 -0600

    Update travis.yml to run all unittest tests. Update models.py to test campsite attributes

--- test_models.py
+++ test_models.py
@@ -1,34 +1,57 @@
-# # test_models.py
-# import unittest
-# import os
-# from app import create_app, db
-# from app.models import Campsite, Amenity
-#
-#
-# class ModelTestCase(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.app = create_app(config_name="testing")
-#         self.client = self.app.test_client
-#         self.campsite = {'name': '18 Road'}
-#         # binds the app to the current context
-#         with self.app.app_context():
-#             # create all tables
-#             db.create_all()
-#
-#
-#     def test_campsite_creation(self):
-#         import pdb; pdb.set_trace()
-#         self.assertEqual(1, 1)
-#
-#
-#     def tearDown(self):
-#         """teardown all initialized variables."""
-#         with self.app.app_context():
-#             # drop all tables
-#             db.session.remove()
-#             db.drop_all()
-#
-# # Make the tests conveniently executable
-# if __name__ == "__main__":
-#     unittest.main()
+# test_models.py
+import unittest
+import os
+from app import create_app, db
+from app.models import Campsite, Amenity, Comment
+
+
+class ModelTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
+        self.campsite = {
+        'name': '18 Rd',
+        'description': 'Biking galore',
+        'city': 'Fruita',
+        'image_url': 'test',
+        'state': 'CO',
+        'driving_tips': 'Turn left then turn right',
+        'lon': 123.2,
+        'lat': 456.6
+        }
+
+        with self.app.app_context():
+            db.create_all()
+
+
+    def test_campsite_creation(self):
+        campsite = Campsite(
+            name = '18 Rd',
+            description = 'Biking galore',
+            city = 'Fruita',
+            image_url = 'test',
+            state = 'CO',
+            driving_tips = 'Turn left then turn right',
+            lon = 123.0,
+            lat = 567.0
+            )
+        self.assertEqual(campsite.description, 'Biking galore')
+        self.assertEqual(campsite.name, '18 Rd')
+        self.assertEqual(campsite.city, 'Fruita')
+        self.assertEqual(campsite.image_url, 'test')
+        self.assertEqual(campsite.state, 'CO')
+        self.assertEqual(campsite.driving_tips, 'Turn left then turn right')
+        self.assertEqual(campsite.lon, 123.0)
+        self.assertEqual(campsite.lat, 567.0)
+        
+
+    def tearDown(self):
+        with self.app.app_context():
+            # drop all tables
+            db.session.remove()
+            db.drop_all()
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
